e1c4940bb0da7c7416591188d73f93c37edafc1e
Stuart Mackey <stu.mackey@gmail.com>
2022-02-17 20:25:18 +0000

    Added telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,7 +1,5 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
-
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
@@ -50,16 +48,8 @@ keymap("v", ">", ">gv", opts)
 keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
 keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
 
---keymap("v", "<A-j>", ":m .+1<CR>==", opts)
---keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
--- Visual Block --
--- Move text up and down
---keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
---keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
---keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
---keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
@@ -67,3 +57,6 @@ keymap("v", "p", '"_dP', opts)
 --keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 --keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 --keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+--keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -57,7 +57,7 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-nvim-lua"
-  
+
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
@@ -65,6 +65,10 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
