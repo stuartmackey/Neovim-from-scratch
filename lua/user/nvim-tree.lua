54ce84764e8f319ef8753eab75ed9192dc0f48f3
Stuart Mackey <stu.mackey@gmail.com>
2022-02-23 19:59:56 +0000

    Changed s to no longer do system open

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -88,6 +88,7 @@ nvim_tree.setup {
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
+        { key = "s", cb = tree_cb "vsplit" },
         { key = "v", cb = tree_cb "vsplit" },
       },
     },
